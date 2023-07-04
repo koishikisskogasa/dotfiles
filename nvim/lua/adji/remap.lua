e8e886fb430d391d8b4f5839d3809d36f34ce5ca
miomaowastaken <mnameyou@gmail.com>
2023-07-04 13:34:54 +0000

    hi

--- nvim/lua/adji/remap.lua
+++ nvim/lua/adji/remap.lua
@@ -1,9 +1,10 @@
 vim.g.mapleader = ","
 vim.keymap.set("i", "<leader>.", "<Esc>")
+vim.keymap.set("n", "<leader>e", vim.cmd.q)
 vim.keymap.set("n", "<leader>u", vim.cmd.NvimTreeOpen)
 vim.keymap.set("n", "<leader>t", vim.cmd.terminal)
 
---vim.keymap.set("n", "<leader>h", "vim.cmd.wincmd h")
---vim.keymap.set("n", "<leader>j", vim.cmd.terminal)
---vim.keymap.set("n", "<leader>k", vim.cmd.terminal)
---vim.keymap.set("n", "<leader>l", vim.cmd.terminal)
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
