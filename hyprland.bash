fb511e83e9e682e933cb838b9ad0fd965b7d87cd
miomaowastaken <mnameyou@gmail.com>
2023-07-04 05:19:52 +0000

    A

--- hyprland.bash
+++ hyprland.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# credit https://moewalls.com/tag/touhou-project/ https://moewalls.com/anime/reimu-vs-marisa-touhou-project-live-wallpaper/
+
+mkdir /home/adji/Pictures/wallpaper/
+cp ./reimu-vs-marisa-touhou-project-moewalls.com.mp4 /home/adji/Pictures/wallpaper/.
 cp -r hypr/ /home/adji/.config/.
