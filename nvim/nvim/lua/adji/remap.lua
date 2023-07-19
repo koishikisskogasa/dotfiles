fefe95b14b789db55c84814ed6c2a335bbd76e75
koishikisskogasa <mnameyou@gmail.com>
2023-07-19 14:04:58 +0800

    h

--- nvim/nvim/lua/adji/remap.lua
+++ nvim/nvim/lua/adji/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ","
 vim.keymap.set("i", "<leader>.", "<Esc>")
 vim.keymap.set("n", "<leader>e", vim.cmd.q)
-vim.keymap.set("n", "<leader>u", vim.cmd.NvimTreeOpen)
+vim.keymap.set("n", "<leader>u", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>t", vim.cmd.terminal)
 
 vim.keymap.set("n", "<C-h>", "<C-w>h")
